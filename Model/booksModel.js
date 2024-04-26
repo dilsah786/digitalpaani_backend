f3dd79bd22954bb22d7f4e99d91f893981e14c0c
Dilnawaz <mddilnawazalam7739@gmail.com>
2024-04-26 16:03:08 +0530

    Updated that user can only update and delete thier books

--- Model/booksModel.js
+++ Model/booksModel.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    publicationYear: { type: Number, required: true }
+    publicationYear: { type: Number, required: true },
+    userId:{type:String}
 });
 
 const BooksModel = mongoose.model('Book', bookSchema);
